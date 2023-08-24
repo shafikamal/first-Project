d8d9fac8311c9d9855c00c1b6b03615de3ae09bb
shafikamal <shafikamal712@gmail.com>
2023-08-25 00:58:09 +0600

    First Program

---
+++ test.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>add number</title>
+</head>
+<body>
+	<form action="first class prc.php" method="post" enctype="multipart/form-data">
+		
+	    <input type="file" name="img">
+		<input type="submit" name="submit" value="Save">
+		
+
+
+
+	</form>
+
+</body>
+</html>
